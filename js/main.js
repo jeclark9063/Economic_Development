33d52adfd1502ed5244fb5866c4e40fb02a46bc6
Barbacamanitu <russmbiz@gmail.com>
2017-01-23 19:31:28 -0600

    Added some testing
    Press A to toggle between two types of layers

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
 var cartoUrl = 'http://libs.cartocdn.com/cartodb.js/v3/3.15/cartodb.js';
 var googleUrl = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyCdOV9pkzdv7uo1u2r1h73gunR91uNW_0M';
+var layers = {"a": null, "b": null};
+
+var activeLayer = 0;
+
 
 requirejs([googleUrl],function(){
      requirejs([cartoUrl],function(){
@@ -14,10 +18,28 @@ map = new google.maps.Map(document.getElementById('map'), {
   zoom: 13,
   mapTypeId: google.maps.MapTypeId.ROADMAP
 });  
-makeLayer();
+
+addCartoLayer1();
+
 }
 
-function makeLayer(){
+
+
+function addCartoLayer1(){
+
+    cartodb.createLayer(map, "https://cartomike.carto.com/api/v2/viz/92b6a26e-a3c9-11e6-a4a5-0ecd1babdde5/viz.json")
+    .done(function(layer) {
+        map.overlayMapTypes.setAt(0, layer);
+        layers.a = layer;
+        addCartoLayer2();
+
+    }).error(function(layer) {
+        console.log('problem loading cartodb layer');
+    });
+}
+
+function addCartoLayer2(){
+    
     cartodb.createLayer(map, {
         user_name: 'cartomike',
         type: 'cartodb',
@@ -27,10 +49,47 @@ function makeLayer(){
             cartocss: '#parcels_carto{   polygon-fill: #FF6600;   polygon-opacity: 0;   line-color: #ff8000;   line-width: 1.0;   line-opacity: 1.0;   [zoom < 15] {line-width: 0.5}   [zoom < 15] {line-opacity: 0.5}   [zoom < 13] {line-opacity: 0.15}   [zoom > 17] {line-width: 2.5} }'
         }]
     })
-    .addTo(map)
     .done(function(layer) {
-        console.log('Loaded carto layer'); // still ==1
+        map.overlayMapTypes.setAt(1, layer);
+        layers.b = layer;
+        doneLoading();
     }).error(function(layer) {
         console.log('problem loading cartodb layer');
     });
+}
+
+///Testing stuff
+function doneLoading(){
+    console.log('Done loading both layers');
+    showA();
+}
+
+$(window).keypress(function(event){
+    if (event.which == 97){
+        toggleLayers();
+    }
+});
+
+function toggleLayers(){
+    if (activeLayer === 1){
+        activeLayer = 0;
+        showA();
+    }
+    else
+    {
+        activeLayer = 1;
+        showB();
+    }
+}
+
+function showA(){
+layers.a.show();
+layers.b.hide();
+console.log("Showing layer A (JSON Version)");
+}
+
+function showB(){
+layers.a.hide();
+layers.b.show();
+console.log("Showing layer B (SQL Version)");
 }
